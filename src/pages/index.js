b57b851fee34d6673469411507753d1f25323dc1
TuanDo\PC-TuanDo <tuando1991995@gmail.com>
2024-03-20 22:33:06 +0700

    đổi tên ở trang chủ

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,10 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          {/* {siteConfig.title} */}
+          Dev Tutorial
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">We are Dev</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
